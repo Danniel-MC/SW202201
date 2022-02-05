b2db4daa463d7607cc513302e7cd616b7584f6a8
dannielmc <enriquenavarro315@gmail.com>
2022-02-05 09:59:27 -0600

    20220205 test

---
+++ __test__/dao/dao.test.js
@@ -0,0 +1,13 @@
+const Pacientes = require ("../../dao/pacientes/pacientes.model");
+
+describe("Testing Pacientes Model", () => {
+    let pacientesModel = undefined;
+    beforeAll(() => {
+        return pacientesModel = new Pacientes();
+    });//end beforeAll
+
+    it('pacientesModel esta definido', ()=>{
+        return expect(pacientesModel).toBeDefined();
+    });//end it pacientesModel
+
+}); //end describe
